c9f007e09a396c57960560e5d23d63dc3b17f7ea
Oleksandr Pivtorak <Sanjok150397@gmail.com>
2025-07-24 20:25:23 +0300

    dataclass & 1st ORM created

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,45 @@
 import sqlite3
+from typing import List
 
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self._connection = sqlite3.connect(db_name)
+        self.table_name = table_name
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def all(self) -> List[Actor]:
+        db_cursor = self._connection.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
+        return [Actor(*row) for row in db_cursor]
+
+    def update(
+            self,
+            pk: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
+        pass
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name=?, last_name=? WHERE id=?",
+            (new_first_name, new_last_name, pk)
+        )
+        self._connection.commit()
+
+    def delete(self, pk: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id=?",
+            (pk,)
+        )
+        self._connection.commit()
